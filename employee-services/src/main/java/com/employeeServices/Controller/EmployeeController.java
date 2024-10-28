abff118e68e65ba02210887c82393a72fc813ef6
sagar <sagar@sagar-Inspiron-3593>
2024-10-28 09:47:25 +0530

    implements Circuit Breaker and Retry

--- employee-services/src/main/java/com/employeeServices/Controller/EmployeeController.java
+++ employee-services/src/main/java/com/employeeServices/Controller/EmployeeController.java
@@ -6,10 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,38 +20,48 @@ import com.employeeServices.repo.EmployeeRepo;
 import com.employeeServices.responses.EmployeeResponse;
 import com.employeeServices.service.EmployeeService;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+
 @RestController
+@RequestMapping("/employees")
 public class EmployeeController {
 	
 	@Autowired
 //	private EmployeeRepo employeeRepo;
 	private EmployeeService employeeService;
 	
-	@GetMapping("/employees")
+	@GetMapping("")
 	public List<Employee> getAllEmployees() {
 		List<Employee> employees = employeeService.getAllEmployee();
 		return employees;
 	}
 	
-	@GetMapping("/employees/{id}")
+	int retryCount = 1;
+	@GetMapping("/{id}")
+//	@CircuitBreaker(name = "addressEmployeeBreaker", fallbackMethod = "addressFallBack")
+	@Retry(name = "addressEmployeeRetry", fallbackMethod = "addressFallBack")
 	ResponseEntity<EmployeeResponse> getEmployeeDetails(@PathVariable("id") int id) {
+		System.out.println("----------------------------------------"+retryCount);
+		retryCount+=1;
 		EmployeeResponse employeeResponse =  employeeService.getEmployeeById(id);
 		return ResponseEntity.status(HttpStatus.OK).body(employeeResponse);
 	}
 	
-//	@PostMapping("/employee/add")
-//	ResponseEntity<String> addEmployee() {
-//		Employee emp = new Employee();
-//		emp.setName("suraj");
-//		emp.setEmail("suraj@gmail.com");
-//		emp.setBloodGroup("b -ve");
-//		employeeService.insertEmployee(emp);
-//		return ResponseEntity.status(HttpStatus.CREATED).body("Successfully Created");
-//	}
+	ResponseEntity<EmployeeResponse> addressFallBack(int id, Exception ex){
+		EmployeeResponse employeeResponse = new EmployeeResponse();
+		return ResponseEntity.status(HttpStatus.OK).body(employeeResponse);  
+	}
 	
-	@PostMapping("/employees/add")
+	@PostMapping("/add")
+	@Transactional
+	@CircuitBreaker(name = "addressEmployeeBreaker", fallbackMethod = "callBackForAddEmployee")
 	ResponseEntity<String> addEmployee(@RequestBody EmployeeResponse employeeResponse){
 		employeeService.addEmployee(employeeResponse);
 		return ResponseEntity.status(HttpStatus.CREATED).body("Successfully Created");
+	}
+	
+	ResponseEntity<String> callBackForAddEmployee(EmployeeResponse employeeResponse, Exception ex){
+		return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Services is down, try after some time");
 	}
 }
